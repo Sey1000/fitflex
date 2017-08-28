d46260866d6ea3ae58e65b7875b534d6656a7f20
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-28 13:29:05 +0200

    Complete associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :studio
-  has_many :bookings, :reviews
+  has_many :reviews
+  has_many :bookings
+  has_many :users, through: :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  # TODO: change this line once Instructor is added
+  belongs_to :studio
+
+  has_many :bookings
+  has_many :reviews
+  has_many :courses, through: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
