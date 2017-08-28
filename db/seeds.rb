aea2c5fa1451f64019a84592d42b3c2207266f64
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-28 16:47:40 +0200

    Complete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
 require 'faker'
 
-puts "Starts seeding.."
+puts "Start seeding.."
 
 # First clear database 
-Studio.destroy_all
+Review.destroy_all
+Booking.destroy_all
+Course.destroy_all
 User.destroy_all
+Studio.destroy_all
 
 # Seed 10 Studios
-puts "Seeding 10 Studios"
+puts "Created 10 Studios"
 studio_names = [
   "Bodystreet", "Fitbox", "Urban Gladiator", "Medical Fitness Academy", 
   "McFit", "Ladyline Loft", "Westhaften Studios", "Nautilus Fitnesscenter", 
@@ -38,7 +41,7 @@ studioI = Studio.create(name: studio_names[8], description: studio_descriptions[
 studioJ = Studio.create(name: studio_names[9], description: studio_descriptions[9], address: studio_addresses[9], telephone: studio_phone_numbers[9])
 
 # Seed Users that DON'T belong to studios (regular customers)
-puts "Seeding 20 Users"
+puts "Created 20 Users"
 
 user_emails = [
   "kris@wagon.com", "maren@wagon.com", "sey@wagon.com", "nhung@wagon.com",
@@ -79,35 +82,122 @@ customerT = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.
 customerT.studio = studioA
 customerT.save
 
-puts "Seeding 20 Courses. To get correct course dates, make sure to seed before demo"
+puts "Created 26 Courses. To get correct course dates, make sure to seed before demo"
 course_titles = [
   "Yoga with Alvaro", "Learn Pole Dancing", "Crossfit for life", "Zumba", 
   "Creative Pilates", "Kickboxing just for you", "Taekwondo", "Hotty hot hot Iron" 
 ]
 
+course_categories = ["Yoga", "Dance", "Crossfit", "Pilates", "Kickboxing", "Martial Arts", "HOT"]
+course_levels = ["Beginner", "Intermediate", "Advanced"]
 t = Time.now.beginning_of_day
 
+# Trying my best to match course title and category
 # courses today
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
-courseA = Course.new(title: course_titles.sample)
+courseA = Course.new(title: course_titles[0], category: course_categories[0], start_time: t + 13.hours, end_time: t + 14.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseA.studio = Studio.all.sample
+courseA.save
+courseB = Course.new(title: course_titles[2], category: course_categories[2], start_time: t + 16.hours, end_time: t + 17.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseB.studio = Studio.all.sample
+courseB.save
+courseC = Course.new(title: course_titles[6], category: course_categories[5], start_time: t + 10.5.hours, end_time: t + 11.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseC.studio = Studio.all.sample
+courseC.save
+courseD = Course.new(title: course_titles[1], category: course_categories[1], start_time: t + 13.5.hours, end_time: t + 15.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseD.studio = Studio.all.sample
+courseD.save
+courseE = Course.new(title: course_titles[3], category: course_categories[1], start_time: t + 22.hours, end_time: t + 23.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseE.studio = Studio.all.sample
+courseE.save
+# courses tomorrow
+courseF = Course.new(title: course_titles[0], category: course_categories[0], start_time: t + 1.day + 10.5.hours, end_time: t + 1.day + 11.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseF.studio = Studio.all.sample
+courseF.save
+courseG = Course.new(title: course_titles[0], category: course_categories[0], start_time: t + 1.day + 9.5.hours, end_time: t + 1.day + 10.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseG.studio = Studio.all.sample
+courseG.save
+courseH = Course.new(title: course_titles[0], category: course_categories[0], start_time: t + 1.day + 12.hours, end_time: t + 1.day + 13.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseH.studio = Studio.all.sample
+courseH.save
+courseI = Course.new(title: course_titles[1], category: course_categories[1], start_time: t + 1.day + 12.5.hours, end_time: t + 1.day + 13.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseI.studio = Studio.all.sample
+courseI.save
+courseJ = Course.new(title: course_titles[1], category: course_categories[1], start_time: t + 1.day + 13.hours, end_time: t + 1.day + 14.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseJ.studio = Studio.all.sample
+courseJ.save
+courseK = Course.new(title: course_titles[1], category: course_categories[1], start_time: t + 1.day + 14.hours, end_time: t + 1.day + 15.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseK.studio = Studio.all.sample
+courseK.save
+courseL = Course.new(title: course_titles[2], category: course_categories[2], start_time: t + 1.day + 15.hours, end_time: t + 1.day + 16.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseL.studio = Studio.all.sample
+courseL.save
+courseM = Course.new(title: course_titles[2], category: course_categories[2], start_time: t + 1.day + 15.hours, end_time: t + 1.day + 16.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseM.studio = Studio.all.sample
+courseM.save
+courseN = Course.new(title: course_titles[5], category: course_categories[4], start_time: t + 1.day + 20.hours, end_time: t + 1.day + 21.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseN.studio = Studio.all.sample
+courseN.save
+courseO = Course.new(title: course_titles[3], category: course_categories[1], start_time: t + 1.day + 19.hours, end_time: t + 1.day + 20.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseO.studio = Studio.all.sample
+courseO.save
+courseP = Course.new(title: course_titles[3], category: course_categories[1], start_time: t + 1.day + 19.5.hours, end_time: t + 1.day + 20.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseP.studio = Studio.all.sample
+courseP.save
+courseQ = Course.new(title: course_titles[5], category: course_categories[4], start_time: t + 1.day + 8.hours, end_time: t + 1.day + 9.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseQ.studio = Studio.all.sample
+courseQ.save
+courseR = Course.new(title: course_titles[6], category: course_categories[5], start_time: t + 1.day + 7.hours, end_time: t + 1.day + 8.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseR.studio = Studio.all.sample
+courseR.save
+courseS = Course.new(title: course_titles[4], category: course_categories[3], start_time: t + 1.day + 7.5.hours, end_time: t + 1.day + 8.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseS.studio = Studio.all.sample
+courseS.save
+courseT = Course.new(title: course_titles[4], category: course_categories[3], start_time: t + 1.day + 10.5.hours, end_time: t + 1.day + 11.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseT.studio = Studio.all.sample
+courseT.save
+# courses next 7 days
+courseU = Course.new(title: course_titles[5], category: course_categories[4], start_time: t + 2.day + 10.5.hours, end_time: t + 2.day + 11.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseU.studio = Studio.all.sample
+courseU.save
+courseV = Course.new(title: course_titles[3], category: course_categories[1], start_time: t + 3.day + 14.hours, end_time: t + 3.day + 15.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseV.studio = Studio.all.sample
+courseV.save
+courseW = Course.new(title: course_titles[2], category: course_categories[2], start_time: t + 4.day + 15.hours, end_time: t + 4.day + 16.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseW.studio = Studio.all.sample
+courseW.save
+courseX = Course.new(title: course_titles[6], category: course_categories[5], start_time: t + 5.day + 13.hours, end_time: t + 5.day + 14.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseX.studio = Studio.all.sample
+courseX.save
+courseY = Course.new(title: course_titles[1], category: course_categories[1], start_time: t + 2.day + 20.hours, end_time: t + 2.day + 21.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseY.studio = Studio.all.sample
+courseY.save
+courseZ = Course.new(title: course_titles[0], category: course_categories[0], start_time: t + 3.day + 10.5.hours, end_time: t + 3.day + 11.5.hours, cost: (7..15).to_a.sample, spots: (4..10).to_a.sample, description: Faker::Lorem.paragraph(1), level: course_levels.sample)
+courseZ.studio = Studio.all.sample
+courseZ.save
 
+puts "Created a bunch of bookings"
+Course.all.each do |crs|
+  (1..4).to_a.sample.times do
+    b = Booking.new
+    b.user = User.all.sample
+    b.course = crs
+    b.save
+  end
+end
+
+sample_titles = [
+  "This was great", "Awesome", "Would do it again", "Okay", "Meh", 
+  "Best course ever", "You should definitely do this course!", "Too difficult",
+  "Too easy for me", "Perfect intensity, cost, everything!", "Best instructor ever",
+  "Good deal!", "Loved it!!", "Please bring this course to Frankfurt"
+]
+
+puts "Created 100 reviews"
+100.times do
+  r = Review.new(title: sample_titles.sample, content: Faker::HowIMetYourMother.quote)
+  r.course = Course.all.sample
+  r.user = User.all.sample
+  r.save
+end
 
 puts "Seeding complete"
