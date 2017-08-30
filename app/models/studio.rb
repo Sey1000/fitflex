9af191ede30df70dbd0866e6387edc527def781b
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-30 16:35:11 +0200

    Fix Javascript requrie order

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,6 +1,7 @@
 class Studio < ApplicationRecord
   has_many :users
   has_many :courses
+  
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
   # TODO: When we add Instructors, change association
