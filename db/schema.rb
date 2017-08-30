877b19947976da7bc7ffdfd2047ebe17726d5232
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-30 15:08:34 +0200

    Add review form to review#new page that looks like course#show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828160654) do
+ActiveRecord::Schema.define(version: 20170830114915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20170828160654) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "content"
+    t.text     "content"
     t.string   "title"
     t.integer  "course_id"
     t.integer  "user_id"
