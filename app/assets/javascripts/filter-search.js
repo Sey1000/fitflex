abd9867d6a25d0d08cdd22de7993b659bb0ed579
Kristofer Espinosa <kristofer.espinosa@gmail.com>
2017-08-30 11:37:57 +0200

    wip

--- app/assets/javascripts/filter-search.js
+++ app/assets/javascripts/filter-search.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 
+  // Pick a day
   $(".button-day").on("click", function() {
     $("#chosen-day").html($(this).text());
+  });
+
+
+  // Pick a category
+  $(".swiper-slide").on("click", function() {
+    $("#chosen-category").html($(this).html());
+  });
+
+  // Pick a level
+  $(".button-level").on("click", function() {
+    $("#chosen-level").html($(this).text());
+  });
+
+  // Pick a distance
+  $(".button-distance").on("click", function() {
+    $("#chosen-distance").html($(this).text());
   });
 });
