8705e5362a5745148f4cde89b16a06af2ccc69a3
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-31 08:39:17 +0200

    Remove hello line

--- app/assets/javascripts/user-show.js
+++ app/assets/javascripts/user-show.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
     $(".historical-bookings > h2").on("click", function() {
-      console.log("hello");
       $(this).next(".row").toggleClass("hidden");
     });
 });
