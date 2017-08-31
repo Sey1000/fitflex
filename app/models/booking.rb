3f55c32739bc5d67d36e5ef748bd2dab555475c4
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-31 16:00:05 +0200

    Clear worktree

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,12 @@ class Booking < ApplicationRecord
   belongs_to :course
 
   validates :course, uniqueness: { scope: :user, message: "is already booked by user" }
+
+  before_validation :check_booking_count
+
+  private
+
+  def check_booking_count
+    
+  end
 end
